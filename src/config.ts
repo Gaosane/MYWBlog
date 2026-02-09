3a7b894caab89c119d99267f37d4ff6f213d3d6f
Gaosane <315399537@qqcom>
2026-02-09 21:33:28 +0800

    测试

--- src/config.ts
+++ src/config.ts
@@ -8,23 +8,15 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-<<<<<<< HEAD
+	title: "小马的博客",
+	subtitle: "BLog",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
-=======
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
->>>>>>> b02c826d81f1b4c6656d0af037ed79ce3dcf200f
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-<<<<<<< HEAD
 		enable: true, // Display a banner image on the top of the homepage and post pages
-=======
-		enable: false,
->>>>>>> b02c826d81f1b4c6656d0af037ed79ce3dcf200f
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
